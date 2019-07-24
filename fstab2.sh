91313b2e1568c854447d42a18592946894523b81
malachma <malachma@microsoft.com>
2019-07-24 21:03:25 +0200

    eval test

---
+++ fstab2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+# Here comes the core logic to get a basic fstab only
+
+chroot /mnt/rescue-root << EOF
+mv -f /etc/fstab{,.copy}
+cat /etc/fstab.copy | awk '/\/ /{print}' >> /etc/fstab
+cat /etc/fstab.copy | awk '/\/boot /{print}' >> /etc/fstab
+cat /etc/fstab
+exit
+EOF
+
+
