50d59d232c6049f73c5b06fffd47d0da7f93610d
malachma <malachma@microsoft.com>
2019-07-26 21:08:32 +0200

    EOF corrected

--- fstab2.sh
+++ fstab2.sh
@@ -1,7 +1,8 @@
-chroot /mnt/rescue-root << EOF \
-mv -f /etc/fstab{,.copy} \
-cat /etc/fstab.copy | awk '/\/ /{print}' >> /etc/fstab \
-cat /etc/fstab.copy | awk '/\/boot /{print}' >> /etc/fstab \
-cat /etc/fstab \
-exit \
+chroot /mnt/rescue-root << EOF 
+mv -f /etc/fstab{,.copy} 
+cat /etc/fstab.copy | awk '/\/ /{print}' >> /etc/fstab 
+cat /etc/fstab.copy | awk '/\/boot /{print}' >> /etc/fstab 
+cat /etc/fstab 
+exit 
 EOF
+
