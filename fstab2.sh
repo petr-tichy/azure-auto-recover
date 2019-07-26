16c2d3f57c71f79ac73b583b15e7405bb11feceb
malachma <malachma@microsoft.com>
2019-07-26 21:21:04 +0200

    eval test

--- fstab2.sh
+++ fstab2.sh
@@ -1,4 +1,6 @@
+echo "starting to create chroot environment"
 chroot /mnt/rescue-root << EOF 
+echo "In chroot"
 mv -f /etc/fstab{,.copy} 
 cat /etc/fstab.copy | awk '/\/ /{print}' >> /etc/fstab 
 cat /etc/fstab.copy | awk '/\/boot /{print}' >> /etc/fstab 
