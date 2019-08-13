a1cfebf023481f4d9d46f9b91e71e573b1dd8163
malachma <malachma@microsoft.com>
2019-08-13 17:17:50 +0200

    changed the file to work as here-string

--- fstab2.sh
+++ fstab2.sh
@@ -1,10 +1,11 @@
-echo "starting to create chroot environment"
-chroot /mnt/rescue-root << EOF 
-echo "In chroot"
-mv -f /etc/fstab{,.copy} 
-cat /etc/fstab.copy | awk '/\/ /{print}' >> /etc/fstab 
-cat /etc/fstab.copy | awk '/\/boot /{print}' >> /etc/fstab 
-cat /etc/fstab 
-exit 
-EOF
+#This file contains the logic to create a very minimalist fstab file which allows a boot of the VM
+#in case it contains malformed lines
+#To do this only the root part and if required the boot part will be added to this file
+# The file gets passed over a here-string to chroot, thus the last command is an exit command
+#Each line has also to end with a semicolon to be formating agnostic
 
+mv -f /etc/fstab{,.copy};
+cat /etc/fstab.copy | awk '/\w+\s+\/\s+\w+/ {print}' >> /etc/fstab;
+cat /etc/fstab.copy | awk '/\w+\s+\/boot\s+\w+/ {print}' >> /etc/fstab;
+cat /etc/fstab;
+exit;
