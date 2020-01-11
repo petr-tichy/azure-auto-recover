93d4a138d4662618a8bf97da424a14ad67b3ad30
Marcus Lachmanez <malachma@microsoft.com>
2020-01-11 21:27:44 +0100

    added more meaningfull information at the end of the file. Issue#2

--- fstab.sh
+++ fstab.sh
@@ -5,3 +5,5 @@ awk '/[[:space:]]+\/boot[[:space:]]+/ {print}' /etc/fstab.copy >>/etc/fstab
 #For Suse
 awk '/[[:space:]]+\/boot\/efi[[:space:]]+/ {print}' /etc/fstab.copy >>/etc/fstab
 cat /etc/fstab
+echo "Renaming original file /etc/fstab to /etc/fstab.copy"
+echo "Creating new /etc/fstab file with only /boot and / partitions."
