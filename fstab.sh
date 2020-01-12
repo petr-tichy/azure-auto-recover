2b75fdfc86a57224c827f839c200f5d3ccaabc50
Marcus Lachmanez <malachma@microsoft.com>
2020-01-12 16:36:29 +0100

    boot lines for LVM

--- fstab.sh
+++ fstab.sh
@@ -5,11 +5,11 @@ awk '/[[:space:]]+\/boot[[:space:]]+/ {print}' /etc/fstab.copy >>/etc/fstab
 # For Suse
 awk '/[[:space:]]+\/boot\/efi[[:space:]]+/ {print}' /etc/fstab.copy >>/etc/fstab
 # In case we have a LVM system
-awk '/rootvg-homelv {print}' /etc/fstab.copy >>/etc/fstab
-awk '/rootvg-optlv {print}' /etc/fstab.copy >>/etc/fstab
-awk '/rootvg-tmplv {print}' /etc/fstab.copy >>/etc/fstab
-awk '/rootvg-usrlv {print}' /etc/fstab.copy >>/etc/fstab
-awk '/rootvg-varlv {print}' /etc/fstab.copy >>/etc/fstab
+awk '/rootvg-homelv/ {print}' /etc/fstab.copy >>/etc/fstab
+awk '/rootvg-optlv/ {print}' /etc/fstab.copy >>/etc/fstab
+awk '/rootvg-tmplv/ {print}' /etc/fstab.copy >>/etc/fstab
+awk '/rootvg-usrlv/ {print}' /etc/fstab.copy >>/etc/fstab
+awk '/rootvg-varlv/ {print}' /etc/fstab.copy >>/etc/fstab
 cat /etc/fstab
 echo "Renaming original file /etc/fstab to /etc/fstab.copy"
 echo "Creating new /etc/fstab file with only /boot and / partitions."
