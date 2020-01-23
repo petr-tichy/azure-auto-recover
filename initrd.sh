489a884e9e1bbb3fa0bd6c05c13abec322cda2f4
Marcus Lachmanez <malachma@microsoft.com>
2020-01-23 13:50:49 +0100

    kernel_version is not read from /lib/modules anymore

--- initrd.sh
+++ initrd.sh
@@ -5,13 +5,13 @@
 #
 
 recover_suse() {
-    kernel_version="$(ls -t /lib/modules | head -n 1)"
+    kernel_version=$(rpm -q kernel --last  | head -n 1 | cut -f1 -d' ')
     mkinitrd /boot/initrd-"${kernel_version}" "$kernel_version"
     grub2-mkconfig -o /boot/grub2/grub.cfg
 }
 
 recover_ubuntu() {
-    kernel_version="$(ls -t /lib/modules | head -n 1)"
+    kernel_version=$( zgrep linux-image /var/log/dpkg.log* | grep installed  | cut -d' ' -f5 | cut -d':' -f1 | sed -e 's/linux-image-//' | grep ^[1-9] | sort -V | tail -n 1)
     update-initramfs -k "$kernel_version" -c
     update-grub
 
@@ -21,7 +21,7 @@ recover_ubuntu() {
 # Should handle all redhat based distros
 #
 recover_redhat() {
-    kernel_version="$(ls -t /lib/modules | head -n 1)"
+    kernel_version=$(rpm -q kernel --last  | head -n 1 | cut -f1 -d' ')
     if [[ "$isRedHat6" == "true" ]]; then
         # verify the grub.conf and correct it if needed
         cd "$tmp_dir"
