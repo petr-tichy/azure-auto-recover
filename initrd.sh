4f77322a57bdc3b1e762c1a9c8b48f6257e54ca4
Marcus Lachmanez <malachma@microsoft.com>
2020-02-14 23:33:57 +0100

    kernel_version for redhat. Stripped of the prefix  value. Same for suse

--- initrd.sh
+++ initrd.sh
@@ -5,7 +5,7 @@
 #
 
 recover_suse() {
-    kernel_version=$(sed -e "s/kernel-//;s/.x86_64//" <<< $(rpm -q kernel --last  | head -n 1 | cut -f1 -d' '))
+    kernel_version=$(sed -e "s/kernel-//" <<< $(rpm -q kernel --last  | head -n 1 | cut -f1 -d' '))
     mkinitrd /boot/initrd-"${kernel_version}" "$kernel_version"
     grub2-mkconfig -o /boot/grub2/grub.cfg
 }
@@ -21,7 +21,7 @@ recover_ubuntu() {
 # Should handle all redhat based distros
 #
 recover_redhat() {
-    kernel_version=$(sed -e "s/kernel-//;s/.x86_64//" <<< $(rpm -q kernel --last  | head -n 1 | cut -f1 -d' '))
+    kernel_version=$(sed -e "s/kernel-//" <<< $(rpm -q kernel --last  | head -n 1 | cut -f1 -d' '))
     if [[ "$isRedHat6" == "true" ]]; then
         # verify the grub.conf and correct it if needed
         cd "$tmp_dir"
